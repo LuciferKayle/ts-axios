95ad43b3b14a2a0e809666464e0cf881154de825
oliverwang <long727067136@163.com>
2019-09-06 14:37:00 +0800

    init simple demp

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export interface AxiosRequestConfig {
+  url: string
+  method?: typeMethod
+  data?: any
+  params?: any
+}
+
+export type typeMethod =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
